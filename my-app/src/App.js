bb761fd1e6caad8f69add7e7365e5b28f19d4d5b
J.Z <yiz132@pitt.edu>
2020-01-27 13:57:49 -0500

    start

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathVisualizer from './PathVisualizer/PathVisualizer'
+
+function App() {
+  return (
+    <div className = 'App'>
+      <PathVisualizer></PathVisualizer>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/PathVisualizer/PathVisualizer.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+
+import './PathVisualizer.css';
+
+export default class PathVisualizer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            nodes:[],
+        };
+    }
+
+    componentDidMount(){
+        const nodes = [];
+        for (let r = 0; r < 15; r ++){
+            const curr_row = [];
+            for (let c = 0; c < 64; c++){
+                curr_row.push([]);
+            }
+            nodes.push(curr_row);
+        }
+        this.setState({nodes});
+    }
+
+    render(){
+        const {nodes} = this.state;
+        console.log(nodes);
+        return <div>
+            Shortest Path Visualizer
+            <Node></Node>
+        </div>
+    }
+}
